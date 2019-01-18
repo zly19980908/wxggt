8116b61291adacba6d0d63d41f922c0b0b02d929
cj19990510 <1875763391@qq.com>
2019-01-18 09:45:02 +0800

    test

--- pages/first/first.js
+++ pages/first/first.js
@@ -1,66 +1,116 @@
 // pages/first/first.js
 Page({
+  data: {
+    bordercolor: "rgb(211, 66, 22)",
+    v1: "view3text1",
+    v2: "",
+    v3: "",
+  },
+  changeText: function(event) {//热门话题控制
+    var that=this;
+    var c = event.currentTarget.id;
+    console.log(c);
+    that.setData({
+      v1: "",
+      v2: "",
+      v3: "",
+    });
+    if(c=="v1")
+    {
+      that.setData({
+        v1: "view3text1",
+      });
+     
+    }
+    else if(c=="v2")
+    {
+      that.setData({
+        v2: "view3text1",
+      });
+    }
+    else
+    {
+      that.setData({
+        v3: "view3text1",
+      });
+    }
 
+  },
+  onPullDownRefresh: function () {
+
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+
+    //模拟加载
+
+    setTimeout(function () {
+
+      // complete
+
+      wx.hideNavigationBarLoading() //完成停止加载
+
+      wx.stopPullDownRefresh() //停止下拉刷新
+
+    }, 1500);
+
+  },
   /**
    * 页面的初始数据
    */
-  data: {
-
-  },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    var that = this;
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
+
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
